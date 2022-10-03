6c92564476d018b5d089680791ab05893c767027
Smak0001 <smak0001@hz.nl>
2022-10-03 15:56:53 +0200

    Done

--- starter-code/assets/js/app.js
+++ starter-code/assets/js/app.js
@@ -2,16 +2,34 @@
 console.log('JavaScript is working!');
 const username = window.prompt("What's your name?");
 const age = window.prompt("What's your age?");
+let height = 0;
+let weight = 0;
 
 if (age < 18) {
-	window.alert("Sorry");
+  document.getElementById("advice-text").innerHTML = "Sorry " + username + ", you are not old enough to get a proper advice from us";
+  document.getElementById("advice-card").className = 'box has-background-grey-light';
 } else {
-	height = window.prompt("What's your height in meters? (like 1.76)");
-	weight = window.prompt("What's your weight in kilograms? (like: 86.4)");
+  height = window.prompt("What's your height in meters? (like 1.76)");
+  weight = window.prompt("What's your weight in kilograms? (like: 86.4)");
 }
 
 document.getElementById("username").innerHTML = username;
 document.getElementById("age").innerHTML = age;
 document.getElementById("height").innerHTML = height;
 document.getElementById("weight").innerHTML = weight;
-console.log(height);
+
+const bmi = weight / Math.pow(height, 2);
+
+if (bmi < 18.5) {
+  document.getElementById("advice-text").innerHTML = "Your BMI is: " + bmi + "<br> You have under weight <br> Start with a personal trainer";
+  document.getElementById("advice-card").className = 'box has-background-warning';
+} else if (bmi >= 18.5 && bmi < 25.0) {
+  document.getElementById("advice-text").innerHTML = "Your BMI is: " + bmi + "<br> You have a normal weight <br> Start with any programme";
+  document.getElementById("advice-card").className = 'box has-background-success';
+} else if (bmi >= 25.0 && bmi < 30.0) {
+  document.getElementById("advice-text").innerHTML = "Your BMI is: " + bmi + "<br> You are slightly over weight <br> Start with cardio training";
+  document.getElementById("advice-card").className = 'box has-background-warning';
+} else if (bmi >= 30.0) {
+  document.getElementById("advice-text").innerHTML = "Your BMI is: " + bmi + "<br> You are obese <br> Start with a personal trainer";
+  document.getElementById("advice-card").className = 'box has-background-danger';
+}
